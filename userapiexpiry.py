5fbce9e5c98461bd08165468af2b35a999d72787
Tim Jarrett <tjarrett@veracode.com>
2020-06-22 17:23:02 -0400

    refactor to handle paged queries

--- userapiexpiry.py
+++ userapiexpiry.py
@@ -4,40 +4,23 @@ import datetime as dt
 from dateutil.parser import parse
 from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
 
+from helpers import api
 
-#api_base = "https://api.veracode.com/appsec/v1"
-api_base = "https://api.veracode.com/api/authn/v2"
-headers = {"User-Agent": "Python HMAC Example"}
+def main():
 
+    data = api.VeracodeAPI().get_users()
 
-if __name__ == "__main__":
+    for user in data:
+        data2 = api.VeracodeAPI().get_user(user["user_id"])
 
-    try:
-        response = requests.get(api_base + "/users", auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
-    except requests.RequestException as e:
-        print("Whoops!")
-        print(e)
-        sys.exit(1)
+        if "api_credentials" in data2:
+            date_time_str = parse(data2["api_credentials"]["expiration_ts"])
+            date = date_time_str.date()
+            time = date_time_str.time()
+            print("User: "+ user["user_name"] + " API Credentials expiration date is "+ str(date) + " " + str(time))
+        else:
+            print("User: "+ user["user_name"] + " has no API credentials")
+        
 
-    if response.ok:
-        data = response.json()
-        for user in data["_embedded"]["users"]:
-            try:
-                response2 = requests.get(api_base + "/users/" + user["user_id"], auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
-            except requests.RequestException as e2:
-                print("Whoops!")
-                print(e2)
-                sys.exit(1)
-            if response2.ok:
-                data2 = response2.json()
-                if "api_credentials" in data2:
-                    date_time_str = parse(data2["api_credentials"]["expiration_ts"])
-                    date = date_time_str.date()
-                    time = date_time_str.time()
-                    print("User: "+ user["user_name"] + " API Credentials expiration date is "+ str(date) + " " + str(time))
-                else:
-                    print("User: "+ user["user_name"] + " has no API credentials")
-            else:
-                print(response2.status_code)
-    else:
-        print(response.status_code)
+if __name__ == '__main__':
+    main()
