2c83652387b684a4574a8911b9e96ffa6c8eed7d
Tim Jarrett <tjarrett@veracode.com>
2020-09-03 13:57:09 -0400

    move to veracode-api-py

--- userapiexpiry.py
+++ userapiexpiry.py
@@ -1,13 +1,12 @@
 import sys
 import requests
-import datetime as dt
+import datetime
 from dateutil.parser import parse
-from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
 
-from helpers import api
+from veracode_api_py import VeracodeAPI as vapi
 
 def creds_expire_days_warning():
-    creds = api.VeracodeAPI().get_creds()
+    creds = vapi().get_creds()
     exp = datetime.datetime.strptime(creds['expiration_ts'], "%Y-%m-%dT%H:%M:%S.%f%z")
     delta = exp - datetime.datetime.now().astimezone() #we get a datetime with timezone...
     if (delta.days < 7):
@@ -18,10 +17,10 @@ def main():
     # CHECK FOR CREDENTIALS EXPIRATION
     creds_expire_days_warning()
 
-    data = api.VeracodeAPI().get_users()
+    data = vapi().get_users()
 
     for user in data:
-        data2 = api.VeracodeAPI().get_user(user["user_id"])
+        data2 = vapi().get_user(user["user_id"])
 
         if "api_credentials" in data2:
             date_time_str = parse(data2["api_credentials"]["expiration_ts"])
